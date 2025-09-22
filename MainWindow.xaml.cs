43337754bc77a6a77894fdeda80f0aab3029f72c
Chekov Pavel <asukak@naver.com>
2025-09-22 17:05:44 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSimpleCal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainIcon.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/radiyuSD"));
+        }
+    }
+}
